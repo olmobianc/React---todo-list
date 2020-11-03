5fb861a40c42ac35c5c0285aaf8a49666b646ee5
olmo <olmo1995@gmail.com>
2020-11-03 17:27:18 +0100

    feat: added conditional rendering to completed elements

--- todo/src/todoItem.js
+++ todo/src/todoItem.js
@@ -1,13 +1,20 @@
 import React from "react"
 
 function TodoItem(props) {
+
+    const completedStyles = {
+        fontStyle: "italic",
+        color: "gray",
+        textDecoration: "line-through"
+    }
+
     return (
         <div className="elem-container">
             <input type="checkbox"
                 checked={props.item.completed}
                 onChange={(e) => props.handleChange(e, props.item.id)}
             />
-            <span className="span-container">{props.item.text}</span>
+            <span style = { props.item.completed ? completedStyles : null } className="span-container">{props.item.text}</span>
         </div>
     )
 }
